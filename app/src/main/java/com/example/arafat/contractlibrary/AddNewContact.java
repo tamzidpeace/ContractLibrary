6ecfb221e4b3b430b6e3d5b86629e8d3a4e4244f
techdroweb <45241872+techdroweb@users.noreply.github.com>
2018-12-13 18:18:17 +0600

    Initial commit; Complete Primary Design.

---
+++ app/src/main/java/com/example/arafat/contractlibrary/AddNewContact.java
@@ -0,0 +1,13 @@
+package com.example.arafat.contractlibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddNewContact extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_contact);
+    }
+}
---
+++ app/src/main/java/com/example/arafat/contractlibrary/ContractList.java
@@ -0,0 +1,16 @@
+package com.example.arafat.contractlibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ContractList extends AppCompatActivity {
+
+    private static final String debug = "contractList";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contract_list);
+
+    }
+}
